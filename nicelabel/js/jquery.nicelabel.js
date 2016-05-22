31064e38b9d4a3d119f285163f95f217d2d4e0ef
pjy <731401082@qq.com>
2016-05-22 12:27:26 +0800

    change json parse

--- nicelabel/js/jquery.nicelabel.js
+++ nicelabel/js/jquery.nicelabel.js
@@ -31,7 +31,7 @@
 		return this.each(function(){
 			
 			var obj = $(this),
-				label_opts = toJSON($(this).attr('data-nicelabel') || '{}'), 	//将字符串转化为对象
+				label_opts = JSON.parse($(this).attr('data-nicelabel') || '{}'), 	//将字符串转化为对象
 				
 				position_class,
 				
@@ -153,58 +153,6 @@
 			
 		}
 		return label_html;
-	};
-	/**
-	 * Javascript JSON implementation
-	 * (Parse Method Only)
-	 * https://github.com/douglascrockford/JSON-js/blob/master/json2.js
-	 * Only using for parse method when parsing data-setup attribute JSON.
-	 * @suppress {undefinedVars}
-	 * @namespace
-	 * @private
-	 */
-	var cx = /[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g;
-
-	var toJSON = function (text, reviver) {
-		var j;
-
-		function walk(holder, key) {
-			var k, v, value = holder[key];
-			if (value && typeof value === 'object') {
-				for (k in value) {
-					if (Object.prototype.hasOwnProperty.call(value, k)) {
-						v = walk(value, k);
-						if (v !== undefined) {
-							value[k] = v;
-						} else {
-							delete value[k];
-						}
-					}
-				}
-			}
-			return reviver.call(holder, key, value);
-		}
-		text = String(text);
-		cx.lastIndex = 0;
-		if (cx.test(text)) {
-			text = text.replace(cx, function (a) {
-				return '\\u' +
-					('0000' + a.charCodeAt(0).toString(16)).slice(-4);
-			});
-		}
-
-		if (/^[\],:{}\s]*$/
-				.test(text.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g, '@')
-					.replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']')
-					.replace(/(?:^|:|,)(?:\s*\[)+/g, ''))) {
-
-			j = eval('(' + text + ')');
-
-			return typeof reviver === 'function' ?
-				  walk({'': j}, '') : j;
-		}
-
-		throw new SyntaxError('JSON.parse(): invalid or malformed JSON data');
 	};
 	
 }( jQuery ));
